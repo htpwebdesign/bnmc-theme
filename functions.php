ce845a1ed9f4e6071003410c81e6345ee107c976
Nicole <nicolelogan@hotmail.com>
2025-05-06 14:17:03 -0700

    styling login page

--- functions.php
+++ functions.php
@@ -210,3 +210,30 @@ add_action('wp_dashboard_setup', 'add_walkthrough_dashboard_widget');
 function walkthrough_widget_content() {
     echo '<p><a href="' . esc_url( site_url('/wp-content/uploads/walkthrough.pdf') ) . '" target="_blank">📄 View Site Walkthrough (PDF)</a></p>';
 }
+
+
+// Add custom login logo
+function my_login_logo() { ?>
+  <style type="text/css">
+      #login h1 a {
+          background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/images/site-login-logo.png);
+  height: 65px;
+  width: 320px;
+  background-size: 320px 65px;
+  background-repeat: no-repeat;
+        padding-bottom: 30px;
+      }
+  </style>
+<?php }
+add_action( 'login_enqueue_scripts', 'my_login_logo' );
+
+// Change the login logo URL to the home page
+function my_login_logo_url() {
+  return home_url();
+}
+add_filter( 'login_headerurl', 'my_login_logo_url' );
+
+function my_login_logo_url_title() {
+  return 'Your Site Name and Info';
+}
+add_filter( 'login_headertext', 'my_login_logo_url_title' );
